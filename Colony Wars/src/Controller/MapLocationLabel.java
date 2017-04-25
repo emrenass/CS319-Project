04fa4dd89750e844b733e8686a22ebc46bae6167
emrenass <ahtemrenas@gmail.com>
2017-04-25 19:43:18 +0300

    Updated Draft

---
+++ Colony Wars/src/Controller/MapLocationLabel.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+
+import View.BufferedImageLoader;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author emre
+ */
+
+public class MapLocationLabel extends JLabel {
+
+    private BufferedImage location;
+    BufferedImageLoader loader = new BufferedImageLoader();
+    static int levelNumber = 0;
+    private final String level = "Level ";
+    
+    public MapLocationLabel(){
+        this.location = loader.loadImage("/location.png");
+        setText(level + String.valueOf(++levelNumber));
+        //setSize(new Dimension(64, 104));
+        //setBorder(BorderFactory.createLineBorder(Color.blue,5, true));
+        
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawImage(location, 0, 0, null);
+    }
+    
+}
---
+++ Colony Wars/src/View/TutorialWindow.java
@@ -0,0 +1,8 @@
+package View;
+
+public class TutorialWindow {
+
+	private int tutorialWindowLayout;
+	private int tutorial_Images;
+
+}
