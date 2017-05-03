92f1dfdf55b19ec55ce7fa10164f7bfa09300d01
emrenass <ahtemrenas@gmail.com>
2017-05-04 00:45:14 +0300

    Sound for ubuntu

--- Colony Wars/src/Controller/SoundManager.java
+++ Colony Wars/src/Controller/SoundManager.java
@@ -2,9 +2,11 @@ package Controller;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 import javax.sound.sampled.FloatControl;
 import javax.sound.sampled.LineUnavailableException;
 
@@ -43,12 +45,15 @@ public class SoundManager implements Runnable
         try
         { 
           //URL defaultSound = this.getClass().getResourceAsStream("/Sound/deneme.wav"); 
-          AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(getClass().getResourceAsStream("/Sounds/Intro.wav"));
-          clip = AudioSystem.getClip();
-          clip.open(audioInputStream);
-          clip.loop(Clip.LOOP_CONTINUOUSLY);
-          
-          clip.start();
+            System.out.println(getClass().getResource("/Sounds/Intro.wav"));
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(getClass().getResource("/Sounds/Intro.wav"));
+            AudioFormat format = audioInputStream.getFormat();
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+            clip = (Clip)AudioSystem.getLine(info);
+            clip.open(audioInputStream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+
+            clip.start();
 
         } 
           catch (Exception ex) 
